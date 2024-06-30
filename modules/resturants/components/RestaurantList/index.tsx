d325420ecccff433e38a5ed66e4b9368b8f90213
habib <habib.gcu10@gmail.com>
2024-06-30 13:56:12 -0700

    added restaurant card

--- modules/resturants/components/RestaurantList/index.tsx
+++ modules/resturants/components/RestaurantList/index.tsx
@@ -1,10 +1,18 @@
 "use client";
 import { FC } from "react";
 import useRestaurants from "../../hooks/useRestaurants";
+import RestaurantCard from "../RestaurantCard";
+import styles from "./styles.module.css";
 
 const RestaurantList: FC = () => {
   const { restaurants, status } = useRestaurants();
-  return <div>RestaurantList</div>;
+  return (
+    <div className={styles.root}>
+      {restaurants.map((restaurant) => (
+        <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+      ))}
+    </div>
+  );
 };
 
 export default RestaurantList;
