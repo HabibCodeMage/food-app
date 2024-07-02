6496dec9a85357eebe0a197f35281e7d183a9307
habib <habib.gcu10@gmail.com>
2024-07-01 19:05:30 -0700

    added pagination

--- modules/resturants/components/RestaurantList/index.tsx
+++ modules/resturants/components/RestaurantList/index.tsx
@@ -3,14 +3,40 @@ import { FC } from "react";
 import useRestaurants from "../../hooks/useRestaurants";
 import RestaurantCard from "../RestaurantCard";
 import styles from "./styles.module.css";
+import OutlineButton from "@/modules/common/components/OutlineButton";
+import { useDispatch } from "react-redux";
+import { nextPageWithDelay } from "../../state/restaurants-actions";
+import { AppDispatch } from "@/modules/common/store";
+import { ClipLoader } from "react-spinners";
 
 const RestaurantList: FC = () => {
-  const { restaurants, status } = useRestaurants();
+  const dispatch = useDispatch<AppDispatch>();
+  const { restaurants, status, hasMore } = useRestaurants();
+
+  const onShowMoreClick = () => {
+    dispatch(nextPageWithDelay({ delayMs: 1000 }));
+  };
+
   return (
-    <div className={styles.root}>
-      {restaurants.map((restaurant) => (
-        <RestaurantCard key={restaurant.id} restaurant={restaurant} />
-      ))}
+    <div>
+      <div className={styles.root}>
+        {restaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+        ))}
+      </div>
+      <button onClick={(e) => {}}></button>
+      <div className={styles.showMoreBtnBox}>
+        {hasMore && (
+          <OutlineButton
+            onClick={onShowMoreClick}
+            disabled={status === "loading"}
+            sx={styles.showBox}
+          >
+            {status === "loading" && <ClipLoader color="#ffc700" size={10} />}
+            <p>+Show More</p>
+          </OutlineButton>
+        )}
+      </div>
     </div>
   );
 };
--- modules/resturants/state/restaurants-slice.ts
+++ modules/resturants/state/restaurants-slice.ts
@@ -1,17 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { getRestaurants } from "./restaurants-actions";
+import { getRestaurants, nextPageWithDelay } from "./restaurants-actions";
 import Restaurant from "@/modules/common/interfaces/restaurants.interface";
 
 interface RestaurantsState {
   restaurants: Restaurant[];
   status: "idle" | "loading" | "succeeded" | "failed";
   error?: string;
+  page: number;
+  pageSize: number;
+  hasMore: boolean;
+  _restaurants: Restaurant[];
 }
 
 const initialState: RestaurantsState = {
   restaurants: [],
+  _restaurants: [],
   status: "idle",
   error: undefined,
+  pageSize: 8,
+  hasMore: true,
+  page: 1,
 };
 
 const restaurantsSlice = createSlice({
@@ -24,12 +32,29 @@ const restaurantsSlice = createSlice({
         state.status = "loading";
       })
       .addCase(getRestaurants.fulfilled, (state, action) => {
-        state.restaurants = action.payload;
+        state._restaurants = action.payload;
         state.status = "succeeded";
+        state.hasMore = action.payload.length >= state.pageSize;
+        state.restaurants = action.payload.slice(0, state.pageSize);
+        state.page += 1;
       })
       .addCase(getRestaurants.rejected, (state, action) => {
         state.status = "failed";
         state.error = action.error.message;
+      })
+      .addCase(nextPageWithDelay.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(nextPageWithDelay.fulfilled, (state, action) => {
+        const nextPageRestaurants = action.payload;
+        state.restaurants = [...state.restaurants, ...nextPageRestaurants];
+        state.page += 1;
+        state.hasMore = nextPageRestaurants.length >= state.pageSize;
+        state.status = "succeeded";
+      })
+      .addCase(nextPageWithDelay.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message || "Failed to load next page";
       });
   },
 });
